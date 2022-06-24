79b18e7dc507b7fc33fdfebaaf1ad42be1c0d3cc
doquoctinh <31276748+doquoctinh@users.noreply.github.com>
2022-06-24 16:21:44 +0700

    Add complete project
    Add complete project

---
+++ AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoMapper;
+using Contracts.DataTransferObjects;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace AccountOwnerServer
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Owner, OwnerDto>();
+			CreateMap<Account, AccountDto>();
+			CreateMap<OwnerForCreationDto, Owner>();
+			CreateMap<OwnerForUpdateDto, Owner>();
+		}
+	}
+}
+
